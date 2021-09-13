eee16077e29386e7ce99e6293aa6a17d3425e245
Matt Pichelmayer <pichelmayer@gmail.com>
2021-09-13 15:23:33 -0500

    Update kali_build.sh

--- kali_build.sh
+++ kali_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 APT_PACKAGES="jd-gui" "snapd" "python3-pip" 
-PY_PACKAGES=
+PY_PACKAGES=twint
+
 sudo apt-get update -y && sudo apt-get upgrade
 sudo apt install -y kali-root-login
 apt install -y snapd
