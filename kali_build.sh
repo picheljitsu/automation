8b662effa5ebe203105f4140664c5cc9369aaf02
Matt Pichelmayer <pichelmayer@gmail.com>
2021-09-14 10:36:21 -0500

    Update kali_build.sh

--- kali_build.sh
+++ kali_build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+LAST_CWD=`pwd`
+LOG_FILE=/tmp/kali_setup_install.log
+APT_PACKAGES=(jd-gui snapd python3-pip)
 PY_PACKAGES=twint
 cd /tmp
 sudo apt-get update -y >> install.log 2>&1 && sudo apt-get upgrade >> install.log 2>&1
@@ -10,38 +13,42 @@ sudo apt install -y kali-root-login >> install.log 2>&1
 #logout of the session
 sudo killall -u `whoami`
 
-
-
 #Set root creds
 if [ $1 ]; then
   passwd root $1
 fi 
 
-APT_PACKAGES="jd-gui" "snapd" "python3-pip" 
+
 echo "[*] Starting package install"
-for P in $APT_PACKAGES;
-  do apt install $P -y >> $P_install.log
+for P in $APT_PACKAGES; do
+  echo "[*] $P"
+  apt install $P -y >> $LOG_FILE
   echo "[+] Installed package $P"
 done
 
 #Set up SSH server login
 SSHD=/etc/ssh/sshd_config
 
+echo "[*] Processing ssh daemon rules"
 #Back dataz up
 cp $SSHD $SSHD.bak
-
+echo "[+] Backed up $SSHD"
 #Allow Root Login
 O='#PermitRootLogin prohibit-password'
 I='PermitRootLogin yes'
-egrep -q $TREX $SSHD && sed -i "s/$O/$I/g"  $SSHD 
+egrep -q $O $SSHD && sed -i "s/$O/$I/g" $SSHD && \ 
+        echo "[+] Enabled root login" || \
+        echo "[-] Failed to enable root login"
 
 #Allow pw auth
 O='#PasswordAuthentication yes' 
 I='PasswordAuthentication yes'
-egrep -q $TREX $SSHD && sed -i "s/$O/$I/g"  $SSHD 
+egrep -q $O $SSHD && sed -i "s/$O/$I/g" $SSHD  && \ 
+        echo "[+] Allowed password authentication" || \
+        echo "[-] Failed to allow password authentication"
 
 echo "[*] Restarting services..."
-systemctl restart sshd
+systemctl restart ssh >> $LOG_FILE
 echo "[+] Restarted sshd"
 
 
@@ -49,27 +56,25 @@ netstat -tnlup | egrep "tcp\s.+?22\s.+?LISTEN\s"
 M="[+] SSH Daemon"
 netstat -tnlup | egrep "tcp\s.+?22\s.+?LISTEN\s" 2>&1 > /dev/null && echo "$M running" || echo "$M failed to restart"
 
-systemctl enable --now snapd apparmor
+systemctl enable --now snapd apparmor >> $LOG_FILE
 echo "[+] Started snapd"
 
-pip3 install twint 
+pip3 install twint >> $LOG_FILE
 
 cd /tmp
 
 #https://code.visualstudio.com/docs/setup/linux
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg >> $LOG_FILE
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/ >> $LOG_FILE
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 rm -f packages.microsoft.gpg
-apt install apt-transport-https
-sudo apt update
-sudo apt install code -y
+apt-get install apt-transport-https >> $LOG_FILE 2>&1
+sudo apt-get update >> $LOG_FILE  2>&1
+sudo apt-get install code -y >> $LOG_FILE 2>&1
 
+cd $LAST_CWD
+LOCAL_SBIN=/usr/local/sbin/
 #Set path for custom scripts
 echo "[*] Setting custom scripts..."
-cp SCRIPTS/* /usr/src/
-echo "[+] Copied scripts to /usr/src"
-echo 'PATH=$PATH:/usr/src/' >> ~/.profile
-source ~/.profile
-echo "[+] Added /usr/src to PATH env variable"
-
+cp SCRIPTS/* $LOCAL_SBIN
+echo "[+] Copied scripts to $LOCAL_SBIN"
